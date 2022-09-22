e1956b14edf24fd0c6eb1104fcda0618727e632e
Willian000067 <willian.000067@gmail.com>
2022-09-22 09:50:33 -0700

    Utilizando_controladores_de_texto

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+  TodoListPage({Key? key}) : super(key: key);
+
+  // Codigo para pegar o texto que a pessoa digitou (exemplo: email) e utilizar ele. Depois preciso incluir no SCAFFOLD.
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            //Essa linha abaixo serve para centralizar tudo no meio da tela 'horizontalmente'
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  //Codigo para o botao 'entrar' funcionar
+  void login() {
+    print('Entrar');
   }
 }
