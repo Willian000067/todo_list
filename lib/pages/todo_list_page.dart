f4a3d84afde745784c25a7121067fc8e0f91410b
Willian000067 <willian.000067@gmail.com>
2022-09-22 19:50:39 -0700

    Botao_limpar_tudo

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -9,33 +9,70 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                //O expanded ajuda a deixar o widget ocupando todo espaco disponivel.
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText:
-                        'Ex.: Estudar', //Esse comando serve para deixar uma dica para o usuario.
+              Row(
+                children: [
+                  Expanded(
+                    //O expanded ajuda a deixar o widget ocupando todo espaco disponivel.
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText:
+                            'Ex.: Estudar', //Esse comando serve para deixar uma dica para o usuario.
+                      ),
+                    ),
                   ),
-                ),
+
+                  SizedBox(
+                      width:
+                          8), // Para separa um componente do outro com um espaço no meio.
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
+
               SizedBox(
-                  width:
-                      8), // Para separa um componente do outro com um espaço no meio.
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  padding: EdgeInsets.all(14),
-                ),
-                child: Icon(
-                  Icons.add,
-                  size: 30,
-                ),
-              ),
+                height: 16,
+              ), // Esse codigo serve para realizar o afastamento na VERTICAL.
+
+              Row(
+                children: [
+                  Expanded(
+                    // Expanded serve para fazer com que o componente utilize o maior espaco possivel.
+                    child: Text(
+                      'Voce possui 0 tarefas pendentes!',
+                    ),
+                  ),
+
+                  SizedBox(
+                      width:
+                          8), // Para separa um componente do outro com um espaço no meio.
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Text(
+                      'Limpar tudo',
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
