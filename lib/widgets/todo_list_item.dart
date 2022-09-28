bfe16d613c1723b94206fb12e417684c58ce2de1
Willian000067 <willian.000067@gmail.com>
2022-09-27 21:21:46 -0700

    Criando_os_itens_da_lista

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -6,12 +6,29 @@ class TodoListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[400],
-      height: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+            4), //Para arredondar as bordas da lista de tarefas.
+        color: Colors.grey[200],
+      ),
+      padding: const EdgeInsets.all(16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment
+            .start, //para deixar os itens fixado do lado esquerdo.
         children: [
-          Text('20/11/2015'),
-          Text('Tarefa 123'),
+          Text(
+            '20/11/2015',
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+          Text(
+            'Tarefa 123',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ],
       ),
     );
